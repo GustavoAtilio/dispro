273bc2d56a3e9f1556eea81b0c3d6b2ea1f97c3d
root <atiliodalcanton@gmail.com>
2022-05-14 16:51:24 -0300

    Ajustes tela login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,89 @@
 import type { NextPage } from 'next'
+import Image from 'next/image';
+import Link from 'next/link';
 import { useForm, SubmitHandler } from "react-hook-form";
+import {AiFillEye} from 'react-icons/ai'
+import {RiAlertFill} from 'react-icons/ri'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
 
 type Inputs = {
-  example: string,
-  exampleRequired: string,
+  userName: string;
+  password: string;
 };
 
+const schema = yup.object({
+  userName: yup.string().required('Necessário Usuário'),
+  password: yup.string().required('Necessário Senha'),
+}).required();
 
 const Home: NextPage = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>(
+  {
+    defaultValues: {
+      userName:"",
+      password: ""
+    },
+    resolver: yupResolver(schema)
+  }
+  );
+
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    console.log(data)
+  };
+
   return (
-    <div className={'w-full h-full flex flex-col md:flex-row'}>
+    <div className={'w-full min-h-screen flex'}>
 
-      <div className={'hidden md:block md:w-[50%] bg-sky-700'}>
-          <h1>Logo</h1>
+      <div className={' containerLogo '}>
+          <div className={'mt-[50px] ml-[20px]'}>
+            <Image src={'/assets/disparopro.svg'} width='200' height="70" alt="logo" />
+          </div>
       </div>
 
 
-      <div className={'md:w-[50%] h-screen flex justify-center items-center'}>
-        <div className="w-[45%] h-auto p-12 shadow-md rounded-lg border">
-          <h2 className='my-2 bg-clip-text text-transparent bg-gradient-to-b from-sky-500 to-sky-900 font-bold text-3xl text-center'>Login</h2>
+      <div className={'magicoLogo'}>
+      <div className={'mt-[50px] ml-[20px] logo-mobile'}>
+            <Image src={'/assets/disparopro.svg'} width='200' height="70" alt="logo" />
+          </div>
+        <div className="md:w-[45%] p-12 shadow-md rounded-lg border bg-white"> 
+          <h2 className='my-5 bg-clip-text text-transparent bg-gradient-to-b from-sky-500 to-sky-900 font-bold text-3xl text-center'>Login</h2>
           <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-      
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-      
-      <input type="submit" />
-    </form>
+          <div className={'flex flex-col justify-center input'} >
+              <label className={ errors.userName? 'text-red-500': 'text-cyan-600'}>E-mail ou Celular</label>
+              <input className={'border-2 w-full'} {... register("userName") } />
+          </div> 
+
+          <div className={'flex flex-col justify-center input'} >
+              <label className={ errors.password? 'text-red-500': 'text-cyan-600'}>Senha</label>
+              <div className={'relative'}>
+              <AiFillEye className={'absolute right-2 top-[20%] text-cyan-600'} />
+              <input className={'border-2 w-full'} type={'password'} {... register('password')} />
+              </div>
+          </div> 
+
+          <div className={'flex justify-center'}>
+              <button className={'border-2 bg-blue-500 text-white text-center py-3 px-10 rounded-[10px]'} type={'submit'} >Conectar</button>
+            </div>
+
+            <div className={'flex flex-col items-center my-2'} > 
+              <p className={'text-xs'}>Ainda não é clinte Disparo Pro ?</p>
+              <Link href={"/register"} className={'text-xs text-cyan-600'} >Crie Conta</Link>
+
+            </div>
+          { (errors.userName && errors.password) &&  <div className={'flex flex-row bg-red-300 py-2 rounded-[10px]'}>
+              <div className={'border-l-2 flex border-l-red-700 py-2 w-[40px]'} >
+                <RiAlertFill className={'text-red-500 '} />
+              </div>
+              <p className={'text-xs'}>Usuário ou senha incorreta<br/>tentar novamente</p>
+            </div> }
+          </form>
+
         </div>
+
       </div>
+
     </div>
   )
 }
